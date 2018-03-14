62b275b8492d5ac38a457feca88a15c92ff610f3
wickedsnitch <romain.entakli@etu.univ-amu.fr>
2018-03-14 15:56:45 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,9 @@ public class Main {
 		String data2 = crunchy.crunch("https://raw.githubusercontent.com/fivethirtyeight/data/master/police-deaths/clean_data.csv");
 		//Wrapper wrap1 = new Wrapper("wrap1");
 		Wrapper wrap2 = new Wrapper("wrap2");
-		//wrap1.createTable1(data1);
-		wrap2.createTable2(data2);
+		//wrap1.createTable(data1);
+		wrap2.readFile("ressources/murder_2016_prelim.csv");
+		wrap2.createTable(data2);
 	}
 
 
--- src/Wrapper.java
+++ src/Wrapper.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -15,101 +17,121 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import com.opencsv.CSVReader;
+
+import java.sql.* ;  // for standard JDBC programs
+import java.math.* ; // for BigDecimal and BigInteger support
+
 public class Wrapper {
 	private String name;
+	
+	private CSVReader reader;
+	
 	public Wrapper(String name) {
+	
+		
 		this.name=name;
 		// TODO Auto-generated constructor stub
 	}
-	public static String readFile( String path ) throws IOException {
-		byte [] encoded = Files . readAllBytes(Paths. get(path));
-		return new String(encoded , StandardCharsets.UTF_8) ;
+	@SuppressWarnings("deprecation")
+	public void readFile( String path ) throws IOException {
+		this.reader = new CSVReader(new FileReader(new File(path)), ',');
+		
+//		byte [] encoded = Files . readAllBytes(Paths. get(path));
+//		return new String(encoded , StandardCharsets.UTF_8) ;
 	}
-	public void createTable1(String data){
+	public void createTable(String data){
+		int i;
+		
 		StringBuilder sb = new StringBuilder (); 
+		
 		sb.append("CREATE TABLE "+ name +"(");
-		String[] lignes = data.split ("\n" );
-		String[] premiereLigne = lignes[0].split(",");
-		sb.append("char[25] " + premiereLigne[0] +",");
-		sb.append("char[25] " + premiereLigne[1] +",");
-		sb.append("integer " + premiereLigne[2] +",");
-		sb.append("integer " + premiereLigne[3] +",");
-		sb.append("integer " + premiereLigne[4] +",");
-		sb.append("char[25] " + premiereLigne[5] + ",");
-		sb.append("char[25] " + premiereLigne[6] +")");
+		
+//		String[] lignes = data.split ("\n" );
+//		String[] premiereLigne = lignes[0].split(",");
+		
+		try {
+			String[] premiereLigne = reader.readNext();
+			
+			
+			for (i = 0; i<premiereLigne.length -1 ; i++)
+				sb.append(premiereLigne[i] + " TEXT, ");
+			
+			sb.append(premiereLigne[i] +" TEXT )" );
+			
+			System.out.println(sb);
+			sendRequest(sb.toString());
 
-		for(int i =1; i<lignes.length;i++){
-			add(lignes[i], name);
+			String[] datas;
+			
+			while((datas = reader.readNext()) != null){
+				add2(datas, name);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
+//		for( i =1; i<lignes.length;i++){
+//			add(lignes[i], name);
+//		}
+		
 	}
-	public void createTable2(String data){
-		StringBuilder sb = new StringBuilder (); 
-		sb.append("CREATE TABLE "+ name +"(");
-		String[] lignes = data.split ("\n" );
-		String[] premiereLigne = lignes[0].split(",");
-		sb.append("char[25] " + premiereLigne[0] +",");
-		sb.append("char[25] " + premiereLigne[1] +",");
-		sb.append("char[25] " + premiereLigne[2] +",");
-		sb.append("char[25] " + premiereLigne[3] +",");
-		sb.append("char[25] " + premiereLigne[4] +",");
-		sb.append("Date " + premiereLigne[5] + ",");
-		sb.append("bool " + premiereLigne[6] +",");// verifier type
-		sb.append("char[25] " + premiereLigne[7] +",");
-		sb.append("char[25] " + premiereLigne[8] +",");
-		sb.append("char[25] " + premiereLigne[9] +")");
 
-		for(int i =1; i<lignes.length;i++){
-			add(lignes[i], name);
-		}
-	}
-	public void createTable3(String data){
-		StringBuilder sb = new StringBuilder (); 
-		sb.append("CREATE TABLE "+ name +"(");
-		String[] lignes = data.split ("\n" );
-		String[] premiereLigne = lignes[0].split(",");
-		sb.append("char[25] " + premiereLigne[0] +",");
-		sb.append("char[25] " + premiereLigne[1] +",");
-		sb.append("char[25] " + premiereLigne[2] +",");
-		sb.append("char[25] " + premiereLigne[3] +",");
-		sb.append("char[25] " + premiereLigne[4] +",");
-		sb.append("Date " + premiereLigne[5] + ",");
-		sb.append("bool " + premiereLigne[6] +",");// verifier type
-		sb.append("char[25] " + premiereLigne[7] +",");
-		sb.append("char[25] " + premiereLigne[8] +",");
-		sb.append("char[25] " + premiereLigne[9] +")");
-
-		for(int i =1; i<lignes.length;i++){
-			add(lignes[i], name);
-		}
-	}
-	public void add(String data, String name){
+	public void add2(String[] data, String name){
 		StringBuilder sb = new StringBuilder ();
-		String[] parts=data.split(",");
+		String[] parts = data;
+		
 		sb.append("INSERT INTO "+ name +" ");
 		sb.append("VALUES (");
-		for(int i = 0; i<parts.length; i++){
+		
+		for(int i = 0; i < parts.length; i++){
 			sb.append(parts[i]);
-			if(i!= parts.length -1)	sb.append(",");
+			if(i != parts.length -1)	sb.append(",");
 		}
+		
 		sb.append(")");
-		System.out.println(sb);
+		System.out.println(sb.toString());
+//		sendRequest(sb.toString());
+	}
+	
+	public void add(String data, String name){
+		StringBuilder sb = new StringBuilder ();
+		String[] parts = data.split(",");
+		
+		sb.append("INSERT INTO "+ name +" ");
+		sb.append("VALUES (");
+		
+		for(int i = 0; i < parts.length; i++){
+//			sb.append("?");
+		
+			sb.append(parts[i]);
+			if(i != parts.length -1)	sb.append(",");
+		}
+		
+		sb.append(")");
+		System.out.println(sb.toString());
+//		sendRequest(sb.toString());
 	}
 	public void sendRequest(String request){
-		try{
-			//step1 load the driver class  
-			Class.forName("oracle.jdbc.driver.OracleDriver");
 
-			//step2 create  the connection object  
-			Connection con  = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","oracle");
+
+		try{
+			//step1ï¿½loadï¿½theï¿½driverï¿½classï¿½ï¿½
+			Class.forName("org.sqlite.JDBC");
+
+			//step2ï¿½createï¿½ï¿½theï¿½connectionï¿½objectï¿½ï¿½
+			Connection con  = DriverManager.getConnection("jdbc:sqlite:test.db");
 
 			Statement stmt = con.createStatement();
 
-			//step4 execute query  
-			ResultSet rs = stmt.executeQuery(request);
-			while(rs.next())
-				System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+			stmt.execute(request);
+			//step4ï¿½executeï¿½queryï¿½ï¿½
+			//ResultSet rs = stmt.executeQuery(request);
+//			while(rs.next())
+//				System.out.println(rs.getInt(1)+"ï¿½ï¿½"+rs.getString(2)+"ï¿½ï¿½"+rs.getString(3));
 
-			//step5 close the connection object  
+			//step5ï¿½closeï¿½theï¿½connectionï¿½objectï¿½ï¿½
 			con.close();
 		}catch(Exception e){System.out.println(e);}
 	}
