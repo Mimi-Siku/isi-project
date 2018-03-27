70cd37550d96d2d267beb8090debb9b6bb496494
Naklam <33487668+Naklam@users.noreply.github.com>
2018-03-27 16:05:16 +0200

    Add files via upload

--- src/modele/Fenetre.java
+++ src/modele/Fenetre.java
@@ -1,5 +1,8 @@
+package modele;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.ComponentOrientation;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
@@ -9,15 +12,12 @@ import java.awt.event.ItemListener;
 import java.util.ArrayList;
 
 import javax.naming.event.NamespaceChangeListener;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
-import org.omg.Messaging.SyncScopeHelper;
-
-
 
 public class Fenetre extends JFrame {
 	private JPanel container = new JPanel();
@@ -30,45 +30,55 @@ public class Fenetre extends JFrame {
 	private JComboBox combo2 = new JComboBox(tab2);
 	private JLabel label2 = new JLabel("Age");
 	
+	String[] tab3 = {"male","female"};
+	private JComboBox combo3 = new JComboBox(tab3);
+	private JLabel label3 = new JLabel("Genre");
 	// Bouton Requete
 	private JButton bouton = new JButton("Lancer Requete");
 
 	public Fenetre(int nbBouton, String[] names){
 		// Base de la Fenetre 
 		this.setTitle("View");
-		this.setSize(1000, 1000);
+		this.setSize(500, 500);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		container.setBackground(Color.white);
 		container.setLayout(new BorderLayout());
-		/* Debut version dynamique
-		for(int i=0;i<nbBouton;i++) {
-			
-			combos.get(i).add(new JComboBox(names.));// Reparer tab2 par tab dynamique
-		}
-		for(JComboBox combo : combos) {
-			combo.setPreferredSize(new Dimension(100, 20));
-		}
-		*/
+
 		// Ajout bouton Ville
 		JPanel top = new JPanel();
 		top.add(label);
 		top.add(combo);
-		container.add(top, BorderLayout.NORTH);
+		container.setLayout(new BoxLayout(top, BoxLayout.PAGE_AXIS));
+		container.add(top);
 		this.setContentPane(container);
 		this.setVisible(true);
 		
-		// Ajouton Bouton Age
+		// Ajout Bouton Age
 		JPanel top2 = new JPanel();
 		top2.add(label2);
 		top2.add(combo2);
-		container.add(top2, BorderLayout.CENTER);
+		container.setLayout(new BoxLayout(top2, BoxLayout.PAGE_AXIS));
+		container.add(top2);
 		this.setContentPane(container);
-		this.setVisible(true); 
+		this.setVisible(true);
+		
+		//Ajout Bouton genre
+		JPanel top3 = new JPanel();
+		top3.add(label3);
+		top3.add(combo3);
+		container.setLayout(new BoxLayout(top3, BoxLayout.PAGE_AXIS));
+		container.add(top3 );
+		this.setContentPane(container);
+		this.setVisible(true);
+		
 		// Ajouter Bouton Requete
 		JPanel panel = new JPanel();
 		panel.add(bouton);
 		panel.setLayout(new FlowLayout());
+		container.add(panel,BoxLayout.X_AXIS );
+		this.setContentPane(container);
+		this.setVisible(true); 
 		
 		// Ecoute bouton Ville
 	    combo.addItemListener(new ItemState());
@@ -110,19 +120,6 @@ public class Fenetre extends JFrame {
 		    } 
 	  }
 	  
-	  /*class ItemAction2 implements ItemListener{
-		  JButton button;// Pour savoir quelle bouton a agit je dois stocker le bouton
-		  Integer requete;	// Boolean pour savoir si c'est le bouton de Requete qui a été activé
-		  public ItemAction2(JButton button, int requete){
-			  this.button=button;
-			  this.requete=requete;
-		  }
-		    public void actionPerformed(ActionEvent e ) {
-		      System.out.println("ActionListener : action sur " + combo.getSelectedItem()); // Changer combo par le bon bouton qui change
-		   //Envoyer la vue la quete
-		      	Fenetre.this.toString(combo.getSelectedItem());// Envoi �a � la Vue OU stock le et creer un bouton qui ordonne l'envoi
-		    }
-		  }*/
 	  
 	public void toString(Object objet) {
 		// TODO Auto-generated method stub
